2b9ec145003d197938b17143b5d782dd4e7344d9
Sean Brown <seanb82592@gmail.com>
2020-05-01 19:26:42 -0500

    Adding Missing Files
    Initial commit was missing them majority of the files intended for it. This commit is adding those files.

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AnimationController : MonoBehaviour
+{
+    Animation test = new Animation();
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimationClip testClip = new AnimationClip();
+        if (testClip.isLooping) print("WARNING: CLIP SET TO LOOP");
+        testClip.ClearCurves();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
